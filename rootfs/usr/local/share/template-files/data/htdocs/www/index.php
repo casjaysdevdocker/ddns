40605031b459547d32c4420296ea20c34ee9d34f
casjay <git-admin@casjaysdev.com>
2023-08-10 19:54:48 -0400

    🗃️ Updated domain names 🗃️
    Dockerfile
    LICENSE.md
    rootfs/usr/local/bin/entrypoint.sh
    rootfs/usr/local/bin/start-ddns.sh
    rootfs/usr/local/share/template-files/data/htdocs/www/index.php

--- rootfs/usr/local/share/template-files/data/htdocs/www/index.php
+++ rootfs/usr/local/share/template-files/data/htdocs/www/index.php
@@ -11,8 +11,8 @@
   <link rel="shortcut icon" href="https://casjaysdev-sites.github.io/static/favicon.ico" />
   <link rel="stylesheet" href="https://casjaysdev-sites.github.io/static/default-css/github-ribbon.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css" integrity="sha384-Bo21yfmmZuXwcN/9vKrA5jPUMhr7znVBBeLxT9MA4r2BchhusfJ6+n8TLGUcRAtL" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="https:///static.casjay.net/default-css/casjaysdev.css">
-  <script src="htps://static.casjay.net/default-js/errorpages/isup.js" async defer></script>
+  <link rel="stylesheet" type="text/css" href="https:///static.casjay.pro/default-css/casjaysdev.css">
+  <script src="htps://static.casjay.pro/default-js/errorpages/isup.js" async defer></script>
   <script src="https://casjaysdev-sites.github.io/static/default-js/errorpages/homepage.js" async defer></script>
   <script src="https://casjaysdev-sites.github.io/static/default-js/errorpages/loaddomain.js" async defer></script>
   <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer></script>
@@ -42,25 +42,25 @@
     <!-- Begin Casjays Developments Footer -->
     Copyright 1999 - 2021
     <br />
-    <a href="https://casjay.net" target="_blank">Casjays Developments</a> and <a href="https://malaks-us.github.com/jason" target="_blank">Jason M. Hempstead-Malak</a><br />
-    Hosting by <a href="https://casjay.net/hosting" target="_blank">Casjays Developments: Hosting</a><br />
-    and powered by <a href="https://casjaysdev.com" target="_blank">CasjaysDev.com</a>
+    <a href="https://casjay.pro" target="_blank">Casjays Developments</a> and <a href="https://malaks-us.github.com/jason" target="_blank">Jason M. Hempstead-Malak</a><br />
+    Hosting by <a href="https://casjay.pro/hosting" target="_blank">Casjays Developments: Hosting</a><br />
+    and powered by <a href="https://casjaysdev.pro" target="_blank">CasjaysDev.com</a>
     <br />
     <br />
     <a href="https://www.patreon.com/casjay" target="_blank"><img src="https://img.shields.io/badge/patreon-donate-orange.svg" border="0" alt="Casjays Patreon Page"></a>
     <a href="https://www.paypal.me/casjaysdev" target="_blank"><img src="https://img.shields.io/badge/Donate-PayPal-green.svg?casjay@yahoo.com" alt="Casjays Paypal Page"></a>
     <br />
     <br />
-    <a href="https://help.casjay.net" target="_blank">CasjaysDev Support</a> |
-    <a href="https://bugs.casjay.net" target="_blank">CasjaysDev Bugs</a> <br />
-    <a href="https://status.casjay.net" target="_blank">System Status</a> |
-    <a href="https://servers.casjay.net" target="_blank">Service Status</a> |
-    <a href="https://status.casjaysdev.com" target="_blank">Complete Status</a> <br />
-    <a href="https://casjaysdev.com/domains.html" target="_blank">Casjays Developments Domains</a>
+    <a href="https://help.casjay.pro" target="_blank">CasjaysDev Support</a> |
+    <a href="https://bugs.casjay.pro" target="_blank">CasjaysDev Bugs</a> <br />
+    <a href="https://status.casjay.pro" target="_blank">System Status</a> |
+    <a href="https://servers.casjay.pro" target="_blank">Service Status</a> |
+    <a href="https://status.casjaysdev.pro" target="_blank">Complete Status</a> <br />
+    <a href="https://casjaysdev.pro/domains.html" target="_blank">Casjays Developments Domains</a>
     <br />
     <br />
     Casjays Developments privacy policy can be found at<br />
-    <a href="https://casjaysdev.com/policy/" target="_blank">https://casjaysdev.com/policy</a>
+    <a href="https://casjaysdev.pro/policy/" target="_blank">https://casjaysdev.pro/policy</a>
     <br />
     <!-- End Casjays Developments Footer -->
 
@@ -114,7 +114,7 @@
               "message": "In accordance with the EU GDPR law this message is being displayed. - ",
               "dismiss": "I Agree",
               "link": "CasjaysDev Policy",
-              "href": "https://casjaysdev.com/policy/"
+              "href": "https://casjaysdev.pro/policy/"
             },
             "type": "opt-out"
           })
