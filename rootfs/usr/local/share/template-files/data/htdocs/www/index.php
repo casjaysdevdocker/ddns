67a84b4a92fcd9397a763c022b43bd27db2445d0
casjay <git-admin@casjaysdev.pro>
2024-09-20 08:23:27 -0400

    🗃️ Committing everything that changed 🗃️
    rootfs/usr/local/share/template-files/data/htdocs/www/index.php

--- rootfs/usr/local/share/template-files/data/htdocs/www/index.php
+++ rootfs/usr/local/share/template-files/data/htdocs/www/index.php
@@ -10,7 +10,8 @@
   <meta name="generator" content="CasjaysDev">
   <link rel="shortcut icon" href="https://casjaysdev-sites.github.io/static/favicon.ico" />
   <link rel="stylesheet" href="https://casjaysdev-sites.github.io/static/default-css/github-ribbon.css" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css" integrity="sha384-Bo21yfmmZuXwcN/9vKrA5jPUMhr7znVBBeLxT9MA4r2BchhusfJ6+n8TLGUcRAtL" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css"
+    integrity="sha384-Bo21yfmmZuXwcN/9vKrA5jPUMhr7znVBBeLxT9MA4r2BchhusfJ6+n8TLGUcRAtL" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="https:///static.casjay.pro/default-css/casjaysdev.css">
   <script src="https://static.casjay.pro/default-js/errorpages/isup.js" async defer></script>
   <script src="https://casjaysdev-sites.github.io/static/default-js/errorpages/homepage.js" async defer></script>
@@ -23,15 +24,16 @@
 </head>
 
 <!-- Begin Body -->
+
 <body>
   <!-- Begin Main Body -->
   <div class="container pt-3">
     <h1>Welcome</h1>
-      Your dynamic dns server has been setup<br />
-      Your config files can be found in the /config directory.<br />
-      Your data files can be found in the /data directory.<br />
-      Your web files can be found in the /data/htdocs/www directory.<br />
-      and your log files can be found in /data/logs<br />
+    Your dynamic dns server has been setup<br />
+    Your config files can be found in the /config directory.<br />
+    Your data files can be found in the /data directory.<br />
+    Your web files can be found in the /data/htdocs/www directory.<br />
+    and your log files can be found in /data/logs<br />
   </div>
   <!-- End Main End Body -->
 
@@ -42,13 +44,16 @@
     <!-- Begin Casjays Developments Footer -->
     Copyright 1999 - 2021
     <br />
-    <a href="https://casjay.pro" target="_blank">Casjays Developments</a> and <a href="https://malaks-us.github.com/jason" target="_blank">Jason M. Hempstead-Malak</a><br />
+    <a href="https://casjay.pro" target="_blank">Casjays Developments</a> and <a
+      href="https://malaks-us.github.com/jason" target="_blank">Jason M. Hempstead-Malak</a><br />
     Hosting by <a href="https://casjay.pro/hosting" target="_blank">Casjays Developments: Hosting</a><br />
-    and powered by <a href="https://casjaysdev.pro" target="_blank">CasjaysDev.com</a>
+    and powered by <a href="https://casjaysdev.pro" target="_blank">CasjaysDev.pro</a>
     <br />
     <br />
-    <a href="https://www.patreon.com/casjay" target="_blank"><img src="https://img.shields.io/badge/patreon-donate-orange.svg" border="0" alt="Casjays Patreon Page"></a>
-    <a href="https://www.paypal.me/casjaysdev" target="_blank"><img src="https://img.shields.io/badge/Donate-PayPal-green.svg?casjay@yahoo.com" alt="Casjays Paypal Page"></a>
+    <a href="https://www.patreon.com/casjay" target="_blank"><img
+        src="https://img.shields.io/badge/patreon-donate-orange.svg" border="0" alt="Casjays Patreon Page"></a>
+    <a href="https://www.paypal.me/casjaysdev" target="_blank"><img
+        src="https://img.shields.io/badge/Donate-PayPal-green.svg?casjay@yahoo.com" alt="Casjays Paypal Page"></a>
     <br />
     <br />
     <a href="https://help.casjay.pro" target="_blank">CasjaysDev Support</a> |
@@ -94,10 +99,11 @@
     <br />
     <!-- Begin EU compliant -->
     <div align="center">
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+      <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
       <script>
-        window.addEventListener("load", function() {
+        window.addEventListener("load", function () {
           window.cookieconsent.initialise({
             "palette": {
               "popup": {
